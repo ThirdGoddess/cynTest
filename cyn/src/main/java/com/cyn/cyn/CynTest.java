32b4d66211ae6b9004fdbfc3a11caa8537cb2d50
NorthernBrain <ofmyhub@gmail.com>
2020-10-24 16:26:42 +0800

    first commit

---
+++ cyn/src/main/java/com/cyn/cyn/CynTest.java
@@ -0,0 +1,15 @@
+package com.cyn.cyn;
+
+/**
+ * Created by Yinan on 2020/10/24 4:17 PM
+ */
+public class CynTest {
+    /**
+     * 计算乘以2后的值（测试）
+     *
+     * @param number
+     */
+    public int calculation(int number) {
+        return number * 2;
+    }
+}
